17aad9df6ed74f1776492a5a94ee08b6b5b85db6
Nael Turlure <nael.turlure@etu.univ-amu.fr>
2023-10-09 21:21:21 +0200

    test

--- blackjack/main.cpp
+++ blackjack/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <map>
+#include <utility> // Inclure pour std::pair
+#include <string>
 
 using namespace std;
 
-int main()
+// Alias pour simplifier le code
+using Dictionary = std::map<int , std::pair<int, string>> ;
+
+// Fonction qui retourne un dictionnaire avec des std::pair en valeur
+Dictionary creerdico()
 {
-    cout << "Hello World!" << endl;
+    Dictionary dico;
+    //on crée les cartes de 2 à roi pour les coeurs
+    dico[2] = std::make_pair(2, "ascii");
+    dico[3] = std::make_pair(3, "ascii");
+    dico[4] = std::make_pair(4, "ascii");
+
+    //ici les carreaux
+
+    //ici les pics
+
+    //ici les trèfles
+
+    return dico;
+}
+
+int main() {
+    Dictionary dico = creerdico();
+    // Utilisation du dictionnaire
+    for (const auto & pair : dico)
+    {
+        cout << pair.second.first << endl ;
+    }
     return 0;
 }
